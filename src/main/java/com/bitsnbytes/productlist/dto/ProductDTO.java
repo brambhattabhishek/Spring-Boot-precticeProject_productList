396f2b95b65feefd40c9d7de817c9c50b54458af
Abhishek Brahmbhatt <brambhattabhishek@gmail.com>
2025-11-13 20:49:56 +0530

    createCategory using postrequest

--- src/main/java/com/bitsnbytes/productlist/dto/ProductDTO.java
+++ src/main/java/com/bitsnbytes/productlist/dto/ProductDTO.java
@@ -1,4 +1,22 @@
 package com.bitsnbytes.productlist.dto;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Long categoryId;
+
+
+
 }
